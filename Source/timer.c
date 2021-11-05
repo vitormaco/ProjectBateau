0d760fe7c25974e5968d8d5ab92a488836c5b2ff
vitormaco <vitorcmcorrea@gmail.com>
2021-11-05 13:19:56 +0100

    Remove unused timer functions

--- Source/timer.c
+++ Source/timer.c
@@ -8,14 +8,6 @@ void Timer_SetupClocks(TIMER_Clock_Type clock)
 	RCC->APB1ENR |= clock;
 }
 
-void Timer_Init(TIM_TypeDef *Timer, int TriggerTimeInMs)
-{
-	// 1sec = 72Mhz/(35999+1) = 2000 clocks with this prescale
-	Timer->PSC = 35999;
-	Timer->ARR = (TriggerTimeInMs * 2) - 1;
-	Timer->DIER |= TIM_DIER_UIE;
-}
-
 void Timer_Init_PWM_Mode(TIM_TypeDef *Timer, int dutyCycleInPercent)
 {
 	Timer->ARR = TIMER_PWM_PERIOD_IN_CLOCKS - 1;
@@ -41,13 +33,4 @@ void Timer_Start(TIM_TypeDef *Timer)
 void Timer_Stop(TIM_TypeDef *Timer)
 {
 	Timer->CR1 &= ~TIM_CR1_CEN;
-}
-
-void TIM2_IRQHandler(void)
-{
-	// clear interruption flag
-	TIM2->SR &= ~TIM_SR_UIF;
-	// does stuff
-	GPIO_Toggle(GPIOC, GPIOC_PC10_PIN);
-	GPIO_Toggle(GPIOA, GPIOA_BOARD_LED_PIN);
 }
