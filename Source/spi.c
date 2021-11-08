960b3503f36ff687da7f586ddb1f874b4b03c390
hanneoyy <hanneoy@gmail.com>
2021-11-08 18:02:21 +0100

    inch inch ça marche

--- Source/spi.c
+++ Source/spi.c
@@ -7,6 +7,16 @@ void SPI_init()
 {
 	RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;
 	SPI1->CR1 |= SPI_CR1_MSTR;
+	
+	SPI1->CR1 |= 0x011 << 3;
+	
+	SPI1->CR1 |= SPI_CR1_CPOL;
+	SPI1->CR1 |= SPI_CR1_CPHA;
+	
+	SPI1->CR1 |= SPI_CR1_SSM;
+	SPI1->CR1 |= SPI_CR1_SSI;
+	
+	SPI1->CR1 |= SPI_CR1_SPE;
 }
 
 char SPI_read_write_message(int address)
