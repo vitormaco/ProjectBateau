c81c7384eb833e1b5a1c2492c3b1ea46f972d05c
PMnerea <75248017+PMnerea@users.noreply.github.com>
2021-11-25 15:58:56 +0100

    Add files via upload

--- Include/spi.h
+++ Include/spi.h
@@ -3,7 +3,14 @@
 
 #include "stm32f10x.h"
 
-void SPI_init(void);
-int SPI_read_write_message(int address, int val);
+void SPI_init();
+char SPI_ReadWrite(char val);
+// Fonctions de gestion du Chip Select 
+void SPI_CS_On(void) ; 
+void SPI_CS_Off(void) ; 
+// Fonction de configuration de l'ADXL345
+void MyADXL_Config(void);
+void MyADXL_SendData(uint8_t reg, uint8_t data);
+void Bordage(void);
 
 #endif
